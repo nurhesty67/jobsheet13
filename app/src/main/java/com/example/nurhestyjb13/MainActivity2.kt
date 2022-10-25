57c5ec345cfca41b4c6c2c3d9e3ee893c3d638b6
anita <anita@gmail.com>
2022-10-25 10:40:39 +0700

    first commit

---
+++ app/src/main/java/com/example/nurhestyjb13/MainActivity2.kt
@@ -0,0 +1,26 @@
+package com.example.nurhestyjb13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var namaView: TextView
+    private lateinit var kelasView: TextView
+    private lateinit var nisView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        namaView = findViewById(R.id.namaView)
+        kelasView = findViewById(R.id.kelasView)
+        nisView = findViewById(R.id.nisView)
+        val extras = intent.extras
+        namaView.text = extras?.getString("nama")
+        kelasView.text = extras?.getString("kelas")
+        nisView.text = extras?.getString("nis")
+    }
+}
